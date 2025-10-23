9529b0ea2ec42b090ee81ad07a6d206634b37d61
Oscar Eduardo Alvarez Rodriguez <oscar.alvarez@mercadolibre.com.co>
2025-10-23 17:33:08 -0500

    Add test

---
+++ src/test/java/com/example/HelloWorldControllerTest.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+
+import jakarta.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@MicronautTest
+class HelloWorldControllerTest {
+
+    @Inject
+    @Client("/")
+    HttpClient httpClient;
+
+    @Test
+    void testHelloWorldController() {
+        var response = httpClient.toBlocking().exchange("/hello", String.class);
+        assertEquals(HttpStatus.OK, response.getStatus());
+        assertEquals("hello world!", response.getBody().orElse(null));
+    }
+}
